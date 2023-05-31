5eccf6c56ca8e3922352ee87e5f14e16d8591a74
Internate <87392271+Nathan8044@users.noreply.github.com>
2023-05-31 15:28:18 +0000

    update

--- Day 1/day_1.js
+++ Day 1/day_1.js
@@ -1,11 +1,58 @@
 
 
+// Prompt for User 
 
-var user_input = prompt("Enter your name:")
+var sugar = prompt("Enter amount of sugar:")
+var chocolate_chip = prompt("Enter amount of chocolate chips:")
+var candy_canes = prompt("Enter amount of Candy Canes:")
+var Flour = prompt("Enter amount of Flour:")
+var vanilla_extract = prompt("Enter amount of vanilla extract (tsp):")
 
-if (user_input )
+// Conversion Factors
+var cups_of_sugar = sugar / (1/2) 
+var cups_of_chocolate_chip = chocolate_chip / (3/4) 
+var cups_of_candy_canes = candy_canes / (5)
+var cups_of_flour = Flour / (3/2)
 
+// Vanilla Extract conversion
+
+var tsp_of_vanilla_extract = vanilla_extract 
+
+
+
+if (cups_of_sugar >= 1) {
+    var amount_of_sugar = Math.floor(cups_of_sugar)
+}
+if (cups_of_chocolate_chip >= 1) {
+    var amount_of_chocolate_chip = Math.floor(cups_of_chocolate_chip)
+}
+if (cups_of_candy_canes >= 1) {
+    var amount_of_candy_canes = Math.floor(cups_of_candy_canes)
+}
+if (cups_of_flour >= 1) { 
+    var amount_of_flour = Math.floor(cups_of_flour)
+ }
+
+if (tsp_of_vanilla_extract >= 1) {
+    var amount_of_vanilla_extract = Math.floor(tsp_of_vanilla_extract)
 }
 
-console.log(hello(user_input))
+if (amount_of_sugar > 1 && amount_of_chocolate_chip && amount_of_candy_canes && amount_of_flour && amount_of_vanilla_extract > 1) {
+    
+    var output = "Tis works"
+} else  {
+    var output = "Tis does not work"
+}
+
+
+
+
+console.log(cups_of_sugar)
+console.log(cups_of_chocolate_chip)
+console.log(cups_of_candy_canes)
+console.log(cups_of_flour)
+console.log(amount_of_vanilla_extract)
+console.log(output)
+
+
 
--- Day 1/day_1_problems.md
+++ Day 1/day_1_problems.md
@@ -8,13 +8,20 @@ One cookie will contain these ingredients:
 
 | Ingredients | Amount | Unit |
 |-------------| ------ | ---- |
-| Sugar | 1/2 | Cups |
+| Sugar | 1/2 | tsp |
 | Chocolate Chip | 3/4 | Cups |
 | Candy Canes | 5 | Cups |
 | Flour | 1 and a half | Cups | 
 | Vanilla Extract | 3 | tsp | 
 
 The user needs prompt for values of how much sugar, chocolate chip, candy canes, flour, vanilla extract. From these values you will determine how much cookies you can make from the inputted values. 
+
+You are also factoring if the user does not input any values at all. For example, if the user inputs that they have 1 tsp of sugar, but no vanilla extract then you cannot make a cookie. **Hint you need to check for 'if' all requirements are met and then you have a cookie. Then calculate how many cookies you can make from that amount. 
+
+### Output: 
+
+>Test
+
 
 
 
