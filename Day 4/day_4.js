8c114b1c47f8631e262529847a917441d8f8f558
Internate <87392271+Nathan8044@users.noreply.github.com>
2023-06-03 17:29:34 +0000

    day 4  finish

--- Day 4/day_4.js
+++ Day 4/day_4.js
@@ -1,12 +1,47 @@
-// Example included in the .md file for Day 4 
 
-const fav_colors = {
 
-    Nathan: "Green",
-    Same: "Blue",
-    John: "Red"
+function computerMove() {
+    
+    var computerChoice = Math.floor(Math.random() * 10);
+
+
+    return computerChoice;
 
 }
 
-console.log(fav_colors.Same)
-console.log(fav_colors["Nathan"])
+const score = {
+
+    win: 0,
+    lose: 0,
+    tie: 0
+
+}
+
+
+function playRound(user_input, computerChoice) {
+
+        var computer_choice = computerChoice
+        if (user_input === computerChoice) {
+
+            output = "You lose! Hehe";
+            score.lose += 1;
+        }else if (user_input  != computerChoice) {
+
+            output = "You win! Yay";
+            score.win += 1; 
+
+        }else if (user_input - computerChoice < 0) {
+
+            output = "You tie! Try again next time";
+            score.tie += 1;
+        }
+
+
+        return computer_choice + " " + output + " " + "The current score is " + "Wins: " + score.win + " Loses: " + score.lose + " Ties: " + score.tie;
+
+        
+
+}
+
+
+
--- Day 4/day_4_problems.md
+++ Day 4/day_4_problems.md
@@ -30,3 +30,13 @@ There are two methods for modifying what is in a object in Javascript
 
 
 ```
+
+
+## Problem 1 
+
+You are a game developer and want to develop a matching game. You will need 9 button and each 9 buttons correlate to a value. After pressing the button the value of the clicked button is saved and a computer function is run with random values of 1-9. If the computer chosen value matches your chosen number, you lose. If it does not, you win. 
+
+For when the match is a tie, if the computer picked number subtracted from your number is less than 0, then it is a tie. If not you win. 
+
+You must also keep track of the score using an Object. In the object you must include the wins, losses, and ties. 
+
